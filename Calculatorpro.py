ec3f20ef06e34c2c03be00f130a5ccbf8803f2fa
IshanIqbal1401 <84223505+IshanIqbal1401@users.noreply.github.com>
2021-06-29 21:35:07 +0530

    Update Calculatorpro.py

--- Calculatorpro.py
+++ Calculatorpro.py
@@ -2,8 +2,7 @@ from logo_calculator import logo
 print(logo)
 
 print('Simple  python CalCulator')
-number_1=int(input('Enter UR First digit Here '))
-number_2=int(input('Enter Second digit here '))
+
 def add_function(number_1,number_2):
     return number_1+number_2
 def sub_function(number_1,number_2):
@@ -23,17 +22,31 @@ def ddiv_function(number_1,number_2):
         return number_2//number_1
     else:
         return number_1//number_2
-    
+
 list_of_operations={'+':add_function,'-':sub_function,'*':mul_function,'/':div_function,'//':ddiv_function}
-for key in list_of_operations:
-    print(key,end=', ')
-operation=input('Input the operation u want to perform  :  ')
-operation_input=list_of_operations[operation]
-answer=operation_input(number_1,number_2)
-print(answer)
-
-
-
-
+def calculator():
+    number_1=int(input('Enter UR First digit Here '))
+    number_2=int(input('Enter Second digit here '))
+    for key in list_of_operations:
+        print(key,end=', ')
+    operation_1=input('Input the operation u want to perform  :  ')
+    operation_input=list_of_operations[operation_1]
+    answer=operation_input(number_1,number_2)
+    print(answer)
+    operation_finished=False
+    while not operation_finished:
+        operation=input('Do u want another operation to be performed on the previous one?\nType Y for yes and N for no ').upper()
+        if operation=='Y':
+            operation_2=input('Enter ur operation here')
+            number=int(input('Enter ur number here '))
+            operation_symbol=list_of_operations[operation_2]
+            second_answer=operation_symbol(answer,number)
+            print(f'Answer is {second_answer}')
+        if operation=='N':
+            operation_finished=True
+            print('\033c')
+            calculator()
+        
+calculator()
 
 
